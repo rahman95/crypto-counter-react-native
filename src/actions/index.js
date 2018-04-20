66d98e92227fa837c4e8f6422e9586666a792361
Rahman Younus <rahman_95@live.co.uk>
2018-04-20 23:11:45 +0100

    added api call and reducer states

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,13 @@
 import { api } from '../utils/constants';
+import { FETCH_COIN_DATA, FETCH_COIN_DATA_SUCCESS, FETCH_COIN_DATA_FAIL } from './types'
 
+export const fetchCoinData = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_COIN_DATA })
+        return api.get('v1/ticker/?limit=10').then(res => {
+            dispatch({ type: FETCH_COIN_DATA_SUCCESS, payload: res.data })
+        }).catch(err => {
+            dispatch({ type: FETCH_COIN_DATA_FAIL, payload: err.data })
+        });
+    }
+}
--- src/reducers/cryptoReducer.js
+++ src/reducers/cryptoReducer.js
@@ -1,3 +1,44 @@
-export default function(state = [], action){
-    return state;
-}
+import { 
+    FETCH_COIN_DATA,
+    FETCH_COIN_DATA_SUCCESS,
+    FETCH_COIN_DATA_FAIL
+} from '../actions/types';
+
+const initialState = {
+    isLoading: false,
+    error: false,
+    errorMessage: null,
+    data: null
+};
+
+const CryptoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_COIN_DATA: {
+            return Object.assign({}, state, {
+                isLoading: true,
+                error: false,
+                errorMessage: null,
+            })
+        }
+        case FETCH_COIN_DATA_SUCCESS: {
+            return Object.assign({}, state, {
+                isLoading: false,
+                error: false,
+                errorMessage: null,
+                data: action.payload
+            })
+        }
+        case FETCH_COIN_DATA_FAIL: {
+            return Object.assign({}, state, {
+                isLoading: false,
+                error: true,
+                errorMessage: action.err,
+            })
+        }
+        default: {
+            return state;
+        }
+    }
+};
+  
+export default LoginReducer;
